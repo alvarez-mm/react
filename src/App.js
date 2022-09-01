f19d658d070d87ba0fee466e1fc816e9f928ddf1
Marianela Alvarez <alvarez_mm@hotmail.com>
2022-08-31 23:20:53 -0300

    contador

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 import NavBar from "./components/NavBar/NavBar.js";
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ImagenInicio from './components/ImagenInicio/ImagenInicio';
+import ItemCount from './components/ItemCount/ItemCount';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-      <ItemListContainer greeting={"Bienvenida/o al lugar que estiliza tu casa"}/>    
+      <ItemListContainer greeting={"Bienvenida/o al lugar que estiliza tu casa"}/>
+      <ItemCount/>   
       <header className="App-header">
       <ImagenInicio/>
       </header>
---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import "./stylesItemCount.css"
+
+const ItemCount = (stock, initial , onAdd) => {
+        const [contador, setContador] = useState (0);
+            const suma = () => {
+                setContador ( contador + 1 )
+            };
+            const resta = () => {
+                setContador (contador - 1 )
+            };
+    return (
+        <>
+            <div className="contador">Contador
+            <h2>{contador}</h2>
+            <button onClick={resta}>-</button>
+            <button onClick={suma}>+</button>    
+            </div>
+        </>
+    );
+};
+
+export default ItemCount;
