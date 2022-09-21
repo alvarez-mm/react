69ce750dc69f19b8c540646dc046ede7b0985938
Marianela Alvarez <alvarez_mm@hotmail.com>
2022-09-21 10:21:38 -0300

    context

--- src/App.js
+++ src/App.js
@@ -4,26 +4,29 @@ import './App.css';
 import NavBar from "./components/NavBar/NavBar.js";
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ImagenInicio from './components/ImagenInicio/ImagenInicio';
-
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import CartWidget from './components/CartWidget/CartWidget';
+import { CartContext } from './Context/CartContext';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <header className="App-header">
-        <NavBar/>
-        </header>
-        <Routes>
-          <Route path='/' element= {<ImagenInicio/>}/>
-          <Route path='/productos' element= {<ItemListContainer/>}/>
-          <Route path='/productos/:categoryId' element= {<ItemListContainer/>}/>
-          <Route path='/item/:id' element= {<ItemDetailContainer/>}/>
-        </Routes>
-                    
-      </div>
-    </BrowserRouter>
+    <CartContext>
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+          <NavBar/>
+          </header>
+          <Routes>
+            <Route path='/' element= {<ImagenInicio/>}/>
+            <Route path='/productos' element= {<ItemListContainer/>}/>
+            <Route path='/productos/:categoryId' element= {<ItemListContainer/>}/>
+            <Route path='/item/:id' element= {<ItemDetailContainer/>}/>
+            <Route path='/cart' element= {<CartWidget/>}/>
+          </Routes>              
+        </div>
+      </BrowserRouter>
+    </CartContext>
   );
 }
 
--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,10 +1,15 @@
 import { FaShoppingCart } from "react-icons/fa";
 import "./stylesCartWidget.css"
+import { Link } from "react-router-dom";
 
+<Link></Link>
 const CartWidget = () => {
     return (
+       
         <div className="carrito">
-            <FaShoppingCart/>
+            <Link to= "/cart">    
+                <FaShoppingCart/>
+            </Link>
         </div>
     );
 };
